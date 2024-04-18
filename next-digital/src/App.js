2b10f8be2e273f6cc5017cd66877412ecad5655d
Antxon Eguiguren <antxonegi@gmail.com>
2024-04-18 19:32:21 +0200

    add app routing

--- next-digital/src/App.js
+++ next-digital/src/App.js
@@ -1,7 +1,18 @@
-import Home from './pages/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import UserDetailPage from './pages/UserDetailPage';
 
 function App() {
-  return <Home />;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/user/:userId" element={<UserDetailPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
